7c177a52e5459001726f583e33f799d32c3ebfb5
Martina <martinaescorial9@gmail.com>
2021-06-27 15:16:43 +0200

    Add project files.

---
+++ UD5E13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UD5E13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sLongitud = "Introduzca la longitud del array";
+            int longitud = Convert.ToInt32(sLongitud);
+            int[] array = new int[longitud];
+
+            Random aleatorio = new Random();
+
+            for (int i =0; i<longitud;i++)
+            {
+                array[i] = aleatorio.Next(1, 11);
+            }
+
+        }
+    }
+}
